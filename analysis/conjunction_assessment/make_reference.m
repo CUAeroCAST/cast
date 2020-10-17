85e636b57119c91b9041e7b282fe733ef3ce0780
TrValade <trva6748@colorado.edu>
2020-10-17 12:23:50 -0600

    whoopsies

--- analysis/conjunction_assessment/make_reference.m
+++
@@ -1,9 +0,0 @@
-function state_vec = make_reference(altitude, mu, rad)
-% generates a reference circular, polar orbit
- r = [0; 0; altitude + rad];
- ecc = @(v) ((dot(v, v) - mu / norm(r))*r - dot(r, v) * v)/mu;
- options = optimoptions(@fsolve, 'Display', 'off');
- V = fsolve(ecc, [rad; 0; 0], options);
- V(V < 1e-3) = 0;
- state_vec = [r; V];
-end
