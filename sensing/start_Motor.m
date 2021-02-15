9344eb3b3984736e3e1f9f93d72ab2fa27a72bd3
Aacorn <trace.valade@gmail.com>
2021-02-15 15:36:49 -0700

    motor pwm fix

--- sensing/start_Motor.m
+++ sensing/start_Motor.m
@@ -2,8 +2,8 @@ function [] = start_Motor(sensorObj)
 %START_MOTOR Summary of this function goes here
 %   Detailed explanation goes here
 
-% Start Motor: a5f0029402c1
-writestr = [165, 240, 2, 148, 2, 193];
+% Start Motor: a5f0020294c1
+writestr = [165, 240, 2, 2, 148, 193];
 if sensorObj.ByteOrder == "little-endian"
  write(sensorObj, writestr,  "uint8") % CURRENTLY 5HZ
 else
