08df156009c59ffe314dad9d694168acbaa6d098
Jason Balke <72892160+MadeInItaly99@users.noreply.github.com>
2021-02-19 14:39:26 -0700

    Test updates
    Updates of homing and limit test

--- micro_controller/arduinoCode/motor_control/limit_switch_test/limit_switch_test.ino
+++ micro_controller/arduinoCode/motor_control/limit_switch_test/limit_switch_test.ino
@@ -1,22 +1,24 @@
-int xEA_minus = 9;
-int xEA_plus = 8;
-int xEB_minus = 7;
-int xEB_plus = 6;
+/*int xEA_minus = 1;
+int xEA_plus = 2;
+int xEB_minus = 3;
+int xEB_plus = 4;*/
 
 int xaState;
 int xaLastState;
 
 int xDriverPUL = 10;
 int xDriverDIR = 9;
-int yDriverPUL = 10;
-int yDriverDIR = 11;
+int yDriverPUL = 7;
+int yDriverDIR = 6;
 
 int xlswitch0 = 51;
 int ylswitch0 = 50;
 
 float xPos;
-int pulseDelay = 100;
+float yPos;
+int pulseDelay = 200;
 int xcounter = 0;
+int ycounter = 0;
 
 
 void setup() {
@@ -31,11 +33,11 @@ void setup() {
   pinMode(xlswitch0, INPUT);
   pinMode(ylswitch0, INPUT);
 
-  pinMode(xEA_minus, INPUT);
+  /*pinMode(xEA_minus, INPUT);
   pinMode(xEA_plus, INPUT);
   pinMode(xEB_minus, INPUT);
-  pinMode(xEB_plus, INPUT);
-  xaLastState = digitalRead(xEA_plus);
+  pinMode(xEB_plus, INPUT);*/
+  // xaLastState = digitalRead(xEA_plus);
    // initialize timer1
   noInterrupts();           // disable all interrupts
   TCCR1A = 0;
@@ -67,14 +69,15 @@ void loop() {
 void homing(){
     // put your main code here, to run repeatedly:
   int xlimitState = digitalRead(xlswitch0);
+  //int ylimitState = digitalRead(xlswitch0);
   digitalWrite(xDriverDIR,LOW);
   while(digitalRead(xlswitch0)==1){
     xlimitState = digitalRead(xlswitch0);
-    if(xlimitState ==1){
-    digitalWrite(xDriverPUL,HIGH);
-    delayMicroseconds(pulseDelay);
-    digitalWrite(xDriverPUL,LOW);
-    delayMicroseconds(pulseDelay);
+    if(xlimitState == 1){
+      digitalWrite(xDriverPUL,HIGH);
+      delayMicroseconds(pulseDelay);
+      digitalWrite(xDriverPUL,LOW);
+      delayMicroseconds(pulseDelay);
     }
 //    xaState = digitalRead(xEA_plus); // Reads the "current" state of the outputA
 //    int xbState = digitalRead(xEB_plus);
@@ -92,6 +95,7 @@ void homing(){
 //    xaLastState = xaState; // Updates the previous state of the outputA with the current state
   }
   xPos = 0;
+  yPos = 0;
   xcounter = 0;
   Serial.write("Homing Complete \n");
   interrupts();          
@@ -102,13 +106,13 @@ void homing(){
 void testRange(){
   digitalWrite(xDriverDIR,HIGH); //switch dir
   float ratio = 0.00000021819;
-  while(xPos<.75){
+  while(xPos<.5){
     digitalWrite(xDriverPUL,HIGH);
     delayMicroseconds(pulseDelay);
     digitalWrite(xDriverPUL,LOW);
     delayMicroseconds(pulseDelay);
-    //xPos = xPos+ratio;
-    xaState = digitalRead(xEA_plus); // Reads the "current" state of the outputA
+    xPos = xPos+ratio;
+    /*xaState = digitalRead(xEA_plus); // Reads the "current" state of the outputA
     int xbState = digitalRead(xEB_plus);
     // If the previous and the current state of the outputA are different, that means a Pulse has occured
     if (xaState != xaLastState){
@@ -123,6 +127,7 @@ void testRange(){
     }
     xaLastState = xaState; // Updates the previous state of the outputA with the current state
   }
-  noInterrupts();           // disable all interrupts
+  noInterrupts();           // disable all interrupts*/
+  }
   Serial.write("Range Testing Complete");
 }
