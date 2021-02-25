70cdeec1a9a1418e7dc226d1c1e292d8b98dfbb7
Jason Balke <72892160+MadeInItaly99@users.noreply.github.com>
2021-02-24 22:53:34 -0700

    EKF fixes
    First pass at debugging the EKF

--- state_estimation/ekf.m
+++ state_estimation/ekf.m
@@ -1,5 +1,5 @@
-function [estimate,estimatorParams] = ekf(yk,time,estimatorParams,xs,ys)
-yk = yk';
+function [estimate,estimatorParams] = ekf(sensorReading,time,estimatorParams,xs,ys)
+yk = [sensorReading(2);sensorReading(1)];
 deltat = time - estimatorParams.currentTime;
 estimatorParams.currentTime = time;
 xk = estimatorParams.filter.State;
@@ -7,20 +7,20 @@ Pk = estimatorParams.filter.StateCovariance;
 % Q = estimatorParams.filter.ProcessNoise(deltat);
 Q = [.001,0;0,.001];
 R = estimatorParams.sensorCovariance;
-f = [0 0 1 0;0 0 0 1;0 0 0 0;0 0 0 0];
+A = [0 0 1 0;0 0 0 1;0 0 0 0;0 0 0 0];
 Omega = [deltat^2/2 0;0 deltat^2/2;deltat 0; 0 deltat];
-F = eye(4)+f*deltat;
-predState = f*xk;
+F = eye(4)+A*deltat;
+predState = F*xk;
 Ppred = F*Pk*F'+Omega*Q*Omega';
 x = xk(1);
 y = xk(2);
-H = [(x-xs)/((x-xs)^2+(y-ys)^2)^(1/2), (y-ys)/((x-xs)^2+(y-ys)^2)^(1/2),0,0;
-    1/((y-ys)*((x-xs)^2/(y-ys)^2+1)), -(x-xs)/((y-ys)^2*((x-xs)^2/(y-ys)^2+1)), 0, 0];
+H = [1/((y-ys)*((x-xs)^2/(y-ys)^2+1)), -(x-xs)/((y-ys)^2*((x-xs)^2/(y-ys)^2+1)), 0, 0;
+    (x-xs)/((x-xs)^2+(y-ys)^2)^(1/2), (y-ys)/((x-xs)^2+(y-ys)^2)^(1/2),0,0];
 if(~any(isnan(yk)))
     Kk = Ppred*H'*inv(H*Ppred*H'+R);
     y_pred = [atan((x-xs)/(y-ys));sqrt((x-xs)^2+(y-ys)^2)];
     corrState = predState+Kk*(yk-y_pred);
-    Pcorr = (eye(4)-Kk*H)*Ppred;
+    Pcorr = (eye(4)-Kk*H)*Ppred*(eye(4)-Kk*H)'+Kk*R*Kk';
 else
     corrState = predState;
     Pcorr = Ppred;
