4ed1371e252835f320b636f43261b442740fb306
Aacorn <trace.valade@gmail.com>
2021-03-08 15:10:22 -0700

    ekf test / live pull

---
+++ main/state_estimator_test.m
@@ -0,0 +1,52 @@
+clear
+clc
+close all
+importCast;
+timestep = 2.5e-4;
+
+file = "expressdata--bigmiss.txt";
+
+datum = readmatrix(file);
+
+len = length(datum);
+
+readings = struct;
+readings.distance = datum(:, 1);
+readings.angle = datum(:, 2);
+estimatorParams = struct;
+estimatorParams.currentTime = 0;
+estimatorParams.filter.MeasurementModel = @(x,xs,y,ys)...
+    [(2*x - 2*xs)/(2*((x - xs)^2 + (y - ys)^2)^(1/2)),...
+    (2*y - 2*ys)/(2*((x - xs)^2 + (y - ys)^2)^(1/2)), 0, 0;
+    -(y - ys)/((x - xs)^2*((y - ys)^2/(x - xs)^2 + 1)),...
+    1/((x - xs)*((y - ys)^2/(x - xs)^2 + 1)), 0, 0]; %Range bearing model
+estimatorParams.filter.MeasurementNoise = [0.025^2,0;0,deg2rad(0.45)^2]; %Range, bearing
+estimatorParams.filter.ProcessNoise = @(dt) 0.01*eye(4); %constant process noise
+estimatorParams.filter.StateCovariance = eye(4); %Initial estimate covariance
+estimatorParams.filter.STM = @(dt) eye(4) + [0,0,1,0;0,0,0,1;0,0,0,0;0,0,0,0]*dt;
+estimatorParams.filter.State = [1.5;0;-1;0];
+collisionEstimate(1) = struct("collisionTime", nan, "predState", nan, "Ppred", nan);
+
+sensorParams = struct;
+[sensorParams.boundingbox.r12, sensorParams.boundingbox.r13,...
+ sensorParams.boundingbox.r24, sensorParams.boundingbox.r43]... 
+ = sensor_Bounds(readings);
+
+n = 1;
+for i = 1  : len/16 - 1
+ sensorParams.sensorObj.UserData.scan = struct("distance", datum(16*(i-1) + 1 : 16*i, 1), "angle", datum(16*(i-1) + 1: 16*i, 2));
+ measurement = filter_scan(sensorParams);
+ time = estimatorParams.currentTime + timestep;
+ if measurement.count > 0
+  [estimate, estimatorParams] = state_estimator([measurement.distance, measurement.angle], time, estimatorParams);
+  collisionEstimate = collision_prediction(estimate, estimatorParams, collisionEstimate);
+  est_vec(n) = estimate;
+  col_vec(n) = collisionEstimate;
+  n = n + 1;
+ else
+   estimate.predState = nan(4,1);
+   estimate.Ppred = nan(4,4);
+   estimate.corrState = nan(4,1);
+   estimate.Pcorr = nan(4,4);
+ end
+end
--- state_estimation/collision_prediction.m
+++ state_estimation/collision_prediction.m
@@ -1,7 +1,7 @@
 function collisionEstimate = collision_prediction(estimate, estimatorParams,collisionEstimate)
 % This function forward predicts the current estimate until we reach point
 % of collision and returns the estimate of the object's state at this point
- if(~any(isnan(estimate.corrState)) && estimate.corrState(2)<0 && estimate.corrState(1) > 0)
+ if(~any(isnan(estimate.corrState), "all") && estimate.corrState(2)<0 && estimate.corrState(1) > 0)
   collisionEstimate.collisionTime = -estimate.corrState(1)/estimate.corrState(2) + ...
                   estimatorParams.currentTime;
   %need to calculate it when we can avoid
