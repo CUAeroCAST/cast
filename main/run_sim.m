125d06758816c92b6517b4a4528491cebeb4520b
Aacorn <trace.valade@gmail.com>
2021-04-19 14:34:49 -0600

    plotting changes in run_sim

--- main/run_sim.m
+++ main/run_sim.m
@@ -41,6 +41,7 @@ for i = 1 : steps
   [maneuver, tManeuver, stateManeuver] = make_maneuver_sim(collisionEstimate, [chiefOrbit(i,:), simParams.mass], timeToCol, guidanceParams);
   if maneuver(3)
    moving = true;
+   n = i;
   end
  end
  estimateStorage(i) = estimate;
@@ -48,21 +49,45 @@ for i = 1 : steps
  measurementStorage(i) = measurement;
 end
 
+estimate = collisionStorage(n);
+estimate.Pcorr = estimate.Ppred;
+estimate.corrState = estimate.predState;
+xmax = estimate.corrState(1)+2*sqrt(estimate.Pcorr(1,1));
+xmin =  estimate.corrState(1)-2*sqrt(estimate.Pcorr(1,1));
+ymax = estimate.corrState(2)+2*sqrt(estimate.Pcorr(2,2));
+ymin =  estimate.corrState(2)-2*sqrt(estimate.Pcorr(2,2));
+satelliteState = chiefOrbit(n, :);
+colState = chiefOrbit(end, :);
+unitRad = satelliteState(1:3)/norm(satelliteState(1:3));
+unitAlong = satelliteState(4:6)/norm(satelliteState(4:6));
+unitCross = cross(unitRad,unitAlong);
+Q = [unitAlong;unitCross;unitRad]';
+minvecTemp = Q * [xmin; ymin; 0];
+maxvecTemp = Q * [xmax; ymax; 0];
+maxvec = max([minvecTemp'; maxvecTemp']);
+minvec = min([minvecTemp'; maxvecTemp']);
+
 [X, Y, Z] = sphere;
-re = 6357*1000;
-X = X*re;
-Y = Y*re;
-Z = Z*re;
+
+X = X*maxvec(1) + colState(1);
+Y = Y*maxvec(2) + colState(2);
+Z = Z*maxvec(3) + colState(3);
 plot3(chiefOrbit(:,1), chiefOrbit(:,2), chiefOrbit(:,3), "LineWidth", 1)
 hold on
 plot3(collisionOrbit(:,1), collisionOrbit(:,2), collisionOrbit(:,3), "LineWidth", 1)
 plot3(stateManeuver(:,1), stateManeuver(:,2), stateManeuver(:,3), "LineWidth", 1)
-surf(X,Y,Z);
-legend("Before Maneuver", "Space Junk", "After Maneuver")
+s = surf(X,Y,Z);
+set(s, "FaceColor", [0,0,0]);
+set(s, "FaceAlpha", 0.25)
+legend("Before Maneuver", "Space Junk", "After Maneuver", "2\sigma Ellipse")
 grid minor
 xlabel("ECEF-X (m)")
 ylabel("ECEF-Y (m)")
 zlabel("ECEF-Z (m)")
+title("Maneuver Comparison Integrated to Time-of-Collision")
+xticks([-100, -50, 0, 50, 100])
+yticks([-50, 0, 50])
+zticks([7578e3 - 10, 7578e3, 7578e3 + 10])
 
 log_struct(estimateStorage, [datapath, filesep, 'estimateStorage'])
 log_struct(collisionStorage, [datapath, filesep, 'collisionStorage'])
